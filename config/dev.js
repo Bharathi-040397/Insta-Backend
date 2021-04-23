70d6cb481da185961bb62e2587eeecaff66c0b46
Bharathi <BharathiVairam040397@gmail.com>
2021-04-23 12:37:54 -0700

    changed

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: 'mongodb+srv://Bharathi:o3hDg3bQ38Z6dBWn@cluster0.zbmzx.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    JWT_TOKEN:'egvdjabeiidaacfweqvbq5'
-}
